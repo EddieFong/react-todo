09e47b6cbde2454d82cec18632463b06c46ce8c6
Eddie Fong <fonged@oocl.com>
2018-12-06 09:19:32 +0800

    fix recheck checkbox

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -9,13 +9,12 @@ export default class TodoInput extends Component {
   }
 
   onclickCheckBoxFunction = () => {
-    let temp = this.refs.active_only.value
+    let temp = this.refs.active_only.checked
     this.props.getAllTodo(temp)
   }
 
-
   render() {
-    this.props.getAllTodo("off")
+    this.props.getAllTodo(false)
     return (
       <div>
         <input ref="input"/>
